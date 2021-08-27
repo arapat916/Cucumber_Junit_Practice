dcba58522248e677f8a7ac707434ae154c8745a9
arapat916 <arapat916@gmail.com>
2021-08-27 10:37:05 -0400

    add search feature

--- src/test/java/com/myproject/step_definitions/searchStepDefinitions.java
+++ src/test/java/com/myproject/step_definitions/searchStepDefinitions.java
@@ -1,11 +1,16 @@
 package com.myproject.step_definitions;
 
+import com.myproject.pages.GooglePage;
 import com.myproject.utilities.Driver;
 import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en_old.Ac;
 import org.junit.Assert;
 
 public class searchStepDefinitions {
+
+    GooglePage googlePage = new GooglePage();
 
     @Given("User is on the google page")
     public void user_is_on_the_google_page() {
@@ -13,12 +18,20 @@ public class searchStepDefinitions {
         Driver.getDriver().get("http://www.google.com");
 
     }
-    @Then("User should see title is google")
-    public void user_should_see_title_is_google() {
-        String expectedTitle = "Google";
-        String actualTitle = Driver.getDriver().getTitle();
+    @When("User input {string} in google search box")
+    public void user_input_in_google_search_box(String string) {
 
-        Assert.assertEquals(expectedTitle,actualTitle);
+        googlePage.search(string);
+
+    }
+    @Then("User should see {string} on page")
+    public void user_should_see_on_page(String ExpectedTitle) {
+
+        String ActualTitle = Driver.getDriver().getTitle();
+
+        Assert.assertTrue(ActualTitle.contains(ExpectedTitle));
+        Driver.closeDriver();
+
 
     }
 
