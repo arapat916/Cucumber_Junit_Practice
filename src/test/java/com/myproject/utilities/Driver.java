02e8c00c815ff738f3b282639a6f558772bf6238
arapat916 <arapat916@gmail.com>
2021-08-31 15:13:43 -0400

    added threadlocal news to achieve parallel testing

--- src/test/java/com/myproject/utilities/Driver.java
+++ src/test/java/com/myproject/utilities/Driver.java
@@ -15,58 +15,61 @@ public class Driver {
     }
 
 
-    private static WebDriver driver;
+    private static ThreadLocal <WebDriver> driverPool = new ThreadLocal<>();
 
 
     public static WebDriver getDriver() {
-        if (driver == null) {
+        if (driverPool.get() == null) {
 
-            String browser = ConfigurationReader.getProperty("browser");
-            switch (browser) {
-                case "chrome":
-                    WebDriverManager.chromedriver().setup();
-                    driver = new ChromeDriver();
-                    break;
-                case "firefox":
-                    WebDriverManager.firefoxdriver().setup();
-                    driver = new FirefoxDriver();
-                    break;
+            synchronized (Driver.class) {
+                String browser = ConfigurationReader.getProperty("browser");
+                switch (browser) {
+                    case "chrome":
+                        WebDriverManager.chromedriver().setup();
+                        driverPool.set(new ChromeDriver());
+                        break;
+                    case "firefox":
+                        WebDriverManager.firefoxdriver().setup();
+                        driverPool.set(new FirefoxDriver());
+                        break;
 
-                case "remote-chrome":
-                    try{
-                    DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                    desiredCapabilities.setBrowserName("chrome");
-                    URL gridURL = new URL("http://3.87.8.245:4444/wd/hub");
-                    driver = new RemoteWebDriver(gridURL,desiredCapabilities);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
+                    case "remote-chrome":
+                        try {
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("chrome");
+                            URL gridURL = new URL("http://3.87.8.245:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridURL, desiredCapabilities));
+                            ;
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
 
-                case "remote-firefox":
-                    try{
-                        DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
-                        desiredCapabilities.setBrowserName("firefox");
-                        URL gridURL = new URL("http://3.87.8.245:4444/wd/hub");
-                        driver = new RemoteWebDriver(gridURL,desiredCapabilities);
-                    } catch (Exception e){
-                        e.printStackTrace();
-                    }
-                    break;
+                    case "remote-firefox":
+                        try {
+                            DesiredCapabilities desiredCapabilities = new DesiredCapabilities();
+                            desiredCapabilities.setBrowserName("firefox");
+                            URL gridURL = new URL("http://3.87.8.245:4444/wd/hub");
+                            driverPool.set(new RemoteWebDriver(gridURL, desiredCapabilities));
+                        } catch (Exception e) {
+                            e.printStackTrace();
+                        }
+                        break;
 
-                default:
-                    throw new RuntimeException("no such a browser!");
+                    default:
+                        throw new RuntimeException("no such a browser!");
+                }
             }
         }
-        return driver;
+        return driverPool.get();
 
     }
 
 
         public static void closeDriver () {
-            if (driver != null) {
-                driver.quit();
-                driver = null;
+            if (driverPool.get() != null) {
+                driverPool.get().quit();
+                driverPool.remove();
             }
         }
     }
